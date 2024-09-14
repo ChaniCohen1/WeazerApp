4066caeec9d5f36f12e6d19107350b317e00c61c
chani <72072ch@gmail.com>
2024-09-14 22:11:59 +0300

    add translates and icons

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -4,13 +4,46 @@ import './WeazerCard.css';
 function WeatherCard({data }) {
   console.log(data);
   
+  const cityDict = {'Eilat' : 'אילת' , 'London' : 'לונדון' , 'Alaska' : 'אלסקה' , 'New York' : 'ניו יורק' ,};
+
+  const weatherDescriptions = {
+    'clear sky': 'שמים בהירים',
+    'few clouds': 'מעט עננים',
+    'scattered clouds': 'עננים מפוזרים',
+    'broken clouds': 'שברי ענן',
+    'overcast clouds': 'שמים מעוננים',
+    'mist': 'ערפל',
+    'smoke': 'עשן',
+    'haze': 'אובך',
+    'dust': 'אבק',
+    'fog': 'ערפל',
+    'sand': 'חול',
+    'ash': 'אפר',
+    'squall': 'סופה קצרה',
+    'tornado': 'טורנדו',
+    'tropical storm': 'סופה טרופית',
+    'hurricane': 'הוריקן',
+    'cold': 'קר',
+    'hot': 'חם',
+    'warm': 'חמים',
+    'cool': 'קריר',
+    'rainy': 'גשום',
+    'drizzle': 'גשם זיקוק',
+    'thunderstorm': 'סופת רעמים',
+    'snow': 'שלג',
+    'hail': 'ברד',
+  };
+  
+  const icon = data.main.temp <= 20 ? '❄️' : (data.main.temp <= 30 ? '⛅️' : '☀️');
+
   return (
     <div className="weather-card">
-      <h2>{data.name}</h2>
-      <p>טמפרטורה: {data.main.temp}°</p>
-      <p>תיאור: {data.weather[0].description}</p>
+      <h2>{cityDict[data.name]}</h2>
+      <h3>{icon}</h3>
+      <h4>{weatherDescriptions[data.weather[0].description]}</h4>
+      <p>טמפרטורה נמדדת: {data.main.temp}°</p>
+      <p>טמפרטורה מורגשת: {data.main.feels_like}°</p>
       <p>לחות: {data.main.humidity}%</p>
-      <p>מרגיש כמו: {data.main.feels_like}°</p>
     </div>
   );
 }
